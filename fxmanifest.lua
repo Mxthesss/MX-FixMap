71e8c23ffe1f6a68671a9f02c0d65313deda9856
Matěj Bednář <bednar.matej2@seznam.cz>
2023-03-21 00:10:55 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+name 'Mxthess Map Fix'
+url 'https://github.com/Mxthesss'
+
+client_script 'mx_client.lua'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}

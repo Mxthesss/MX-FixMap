289cabb18e6ca84e14a28354b03ca5a4e421dfae
Mxthesss <bednar.matej2@seznam.cz>
2023-05-26 22:43:12 +0200

    Update mx_client.lua

--- mx_client.lua
+++ mx_client.lua
@@ -1,4 +1,10 @@
 RegisterCommand("fixmap", function (src, args, raw)
     SetRadarBigmapEnabled(false, false)
-    lib.notify({title = 'MAP', description = 'The map has been fixed!', type = 'success', position = 'top'})
+    lib.notify({
+        title = 'MAP', -- OR (CS - MAPA)
+        description = 'Map was succesfully fixed', -- OR (CS - Mapa byla úspešně opravena)
+        type = 'success'
+    })
 end, false)
+
+print('^5Made By Mxthess^7: ^1'..GetCurrentResourceName()..'^7 started ^2successfully^7...')
